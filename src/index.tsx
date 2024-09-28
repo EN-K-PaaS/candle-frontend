524259683dedbeb24e1aed18b5635a05eba3366c
everyday6 <everyday06@sju.ac.kr>
2024-09-29 08:02:58 +0900

    Feat: MyGoalEditor, MyGoalCreator, MyGoal 파일 생성, tailwind.config.js에 색, 사이즈 추가, pencil-icon 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+=======
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+>>>>>>> 0f3999b (Feat: MyGoalEditor, MyGoalCreator, MyGoal 파일 생성, tailwind.config.js에 색, 사이즈 추가, pencil-icon 추가)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
---
+++ src/pages/MyGoal/components/MyGoalEditor.tsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+
+interface MyGoalEditorProps {
+  no: number;
+  progress: number;
+  deadline: Date;
+  onClose: () => void;
+  editGoal: (no: number, progress: number, deadline: Date) => void;
+  deleteGoal: (no: number) => void;
+}
+
+const MyGoalEditor = (props: MyGoalEditorProps) => {
+  const [progress, setProgress] = useState<number>(props.progress);
+  const [deadline, setDeadline] = useState<Date>(props.deadline);
+
+  const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDeadline(new Date(e.target.value));
+  };
+
+  const handleProgress = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value.startsWith("0") && e.target.value.length > 1) {
+      e.target.value = e.target.value.slice(1);
+    }
+
+    const value = Math.max(0, Math.min(100, Number(e.target.value)));
+    setProgress(value);
+  };
+
+  const handleSubmit = () => {
+    props.editGoal(props.no, progress, deadline);
+    props.onClose();
+  };
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
+      <div className="w-full max-w-lg p-6 bg-white rounded-lg shadow-lg">
+        <h1 className="mb-4 text-lg font-bold">Create New Goal</h1>
+
+        <div className="w-30 h-0.5 my-4 bg-gray-200" />
+
+        <div className="mb-4 mt-7">
+          <label className="block mb-5 text-2xl font-extrabold text-textBlue">
+            진행 정도
+          </label>
+          <input
+            type="number"
+            value={progress}
+            min={0}
+            max={100}
+            onChange={handleProgress}
+            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+            placeholder="Enter goal"
+          />
+        </div>
+
+        <div className="mt-6 mb-6">
+          <label className="block mb-2 text-2xl font-extrabold text-textBlue">
+            목표 날짜
+          </label>
+          <div className="">
+            <input
+              type="date"
+              value={deadline.toISOString().split("T")[0]}
+              onChange={handleDateChange}
+            />
+          </div>
+        </div>
+
+        <div className="flex justify-end">
+          <button
+            onClick={props.onClose}
+            className="px-3 py-1 mr-5 transition-colors bg-white border border-gray-300 rounded-lg text-buttonBlue boreder w-15 hover:bg-buttonBlue hover:text-white"
+          >
+            취소
+          </button>
+
+          <button
+            onClick={handleSubmit}
+            className="px-3 py-1 text-white transition-colors rounded-lg bg-buttonBlue w-15 hover:bg-white hover:text-buttonBlue hover:border hover:border-gray-300 "
+          >
+            수정
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyGoalEditor;
