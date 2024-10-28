7311040ca0b9c112c71c179dee7b5e227bca2fe9
everyday6 <everyday06@sju.ac.kr>
2024-10-29 02:36:11 +0900

    solve conflict

--- src/pages/MyGoal/components/MyGoalItem.tsx
+++ src/pages/MyGoal/components/MyGoalItem.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState } from 'react';
 import { MyGoalItemType } from '../MyGoalContainer';
 import MyGoalEditor from './MyGoalEditor';
@@ -6,6 +7,16 @@ type MyGoalItemProps = {
   myGoals: Array<MyGoalItemType>;
   deleteGoal: (no: number) => void;
   editGoal: (goal: MyGoalItemType) => void;
+=======
+import { useState } from "react";
+import { MyGoalItemType } from "../MyGoalContainer";
+import MyGoalEditor from "./MyGoalEditor";
+
+type MyGoalItemProps = {
+  myGoal: Array<MyGoalItemType>;
+  deleteGoal: (no: number) => void;
+  editGoal: (no: number, progress: number, deadline: Date) => void;
+>>>>>>> e3ca5d8 (Refactor: MyGoal.tsx를 MyGoalItem.tsx로 수정함)
 };
 
 const MyGoalItem = (props: MyGoalItemProps) => {
@@ -22,6 +33,7 @@ const MyGoalItem = (props: MyGoalItemProps) => {
   };
 
   const setStatus = (progress: number) => {
+<<<<<<< HEAD
     if (progress < 100) return 'processing';
     return 'completed';
   };
@@ -29,37 +41,72 @@ const MyGoalItem = (props: MyGoalItemProps) => {
   const setStatusColor = (progress: number) => {
     if (progress < 100) return 'text-gold border border-gold';
     return 'text-darkGreen border border-darkGreen';
+=======
+    if (progress < 100) return "processing";
+    return "completed";
+  };
+
+  const setStatusColor = (progress: number) => {
+    if (progress < 100) return "text-gold border border-gold";
+    return "text-darkGreen border border-darkGreen";
+>>>>>>> e3ca5d8 (Refactor: MyGoal.tsx를 MyGoalItem.tsx로 수정함)
   };
 
   return (
     <div>
+<<<<<<< HEAD
       {props.myGoals.map((item) => (
         <div
           key={item.no}
           className="relative flex items-center w-2/3 h-16 p-4 mx-auto bg-white border border-gray-300 rounded-sm">
           <div className="flex items-center">
             <span className={'text-xs absolute left-7 font-semibold'}>
+=======
+      {props.myGoal.map((item) => (
+        <div
+          key={item.no}
+          className="relative flex items-center w-2/3 h-16 p-4 mx-auto bg-white border border-gray-300 rounded-sm"
+        >
+          <div className="flex items-center">
+            <span className={"text-xs absolute left-7 font-semibold"}>
+>>>>>>> e3ca5d8 (Refactor: MyGoal.tsx를 MyGoalItem.tsx로 수정함)
               {item.goal}
             </span>
           </div>
           <span
             className={
+<<<<<<< HEAD
               'border rounded-lg text-[10px] absolute right-72 font-balck'
             }>
+=======
+              "border rounded-lg text-[10px] absolute right-72 font-balck"
+            }
+          >
+>>>>>>> e3ca5d8 (Refactor: MyGoal.tsx를 MyGoalItem.tsx로 수정함)
             {item.deadline.toLocaleDateString()}
           </span>
 
           <span
             className={`${setStatusColor(
               item.progress
+<<<<<<< HEAD
             )} rounded-xl text-[10px] absolute right-45 p-1 `}>
+=======
+            )} rounded-xl text-[10px] absolute right-45 p-1 `}
+          >
+>>>>>>> e3ca5d8 (Refactor: MyGoal.tsx를 MyGoalItem.tsx로 수정함)
             {setStatus(item.progress)}
           </span>
 
           <div className="absolute w-20 h-1 rounded-lg right-16 bg-neutral-200 bg-pastelBlue">
             <div
               className={`h-1 bg-blue-500 rounded-lg`}
+<<<<<<< HEAD
               style={{ width: `${item.progress}%` }}></div>
+=======
+              style={{ width: `${item.progress}%` }}
+            ></div>
+>>>>>>> e3ca5d8 (Refactor: MyGoal.tsx를 MyGoalItem.tsx로 수정함)
           </div>
 
           <img
@@ -73,7 +120,13 @@ const MyGoalItem = (props: MyGoalItemProps) => {
 
       {isOpen && selectedGoal && (
         <MyGoalEditor
+<<<<<<< HEAD
           goal={selectedGoal}
+=======
+          no={selectedGoal.no}
+          progress={selectedGoal.progress}
+          deadline={selectedGoal.deadline}
+>>>>>>> e3ca5d8 (Refactor: MyGoal.tsx를 MyGoalItem.tsx로 수정함)
           onClose={closeMyGoalEditor}
           deleteGoal={props.deleteGoal}
           editGoal={props.editGoal}
