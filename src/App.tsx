2256ce1397d094da79e9dcfe9cb3ca2a62efa91f
everyday6 <everyday06@sju.ac.kr>
2024-10-29 19:25:57 +0900

    Refactor: App, login

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
 import DailyChecklistContainer from './pages/DailyChecklist/DailyChecklistContainer';
 import MyGoal from './pages/MyGoal/MyGoalContainer';
 import Diary from './pages/Diary/index';
 import EmotionAnalysis from './pages/Emotion_Analysis/index';
 import Community from './pages/Community/Community';
-import NavBar from './components/NavBar';
 
 const App = () => {
   return (
     <Router>
       <div>
-        <NavBar></NavBar>
         <Routes>
+          <Route path="/" element={<Login />} />
           <Route path="/dailyChecklist" element={<DailyChecklistContainer />} />
           <Route path="/myGoal" element={<MyGoal />} />
           <Route path="/diary" element={<Diary />} />
